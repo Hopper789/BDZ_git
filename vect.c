dddc2654ef9740c3bde952e60a69d56405a6b089
Hopper <you@example.com>
2024-04-19 13:11:38 +0300

    modified .c

--- vect.c
+++ vect.c
@@ -1,10 +1,18 @@
 #include <stdio.h>
 
+int fib(int a){
+    if (a == 0 || a == 1)
+        return 1;
+    else
+        return fib(a-1) + fib(a-2);
+
+}
 
 
 int main(){
-
-    printf("Fibonachi: 4 - %d", 3);
+    int tt;
+    scanf("%d", &tt);
+    printf("Fibonachi %d: - %d", tt, fib(tt));
     
     return 0;    
 }
